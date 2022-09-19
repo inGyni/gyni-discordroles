472eca3c27ec0d85c8f081f1670d52ea6655cd35
Ali <47601790+inGyni@users.noreply.github.com>
2022-09-19 20:44:32 +0200

    Update shared.lua

--- shared.lua
+++ shared.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-Config.BotToken = "MTAxMTc2NjMyMTA3MzEwMjk0OA.GzBpkf.jhe7wegxOkpM23YZMseuxrxRpbOCs9QpjiGGkU"
-Config.GuildID = "1017957635615637504"
+Config.BotToken = ""
+Config.GuildID = ""
