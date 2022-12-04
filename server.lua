bfefb06d5f1d12b331ebbc91c412af44a437c4f7
Ali <47601790+inGyni@users.noreply.github.com>
2022-12-04 20:39:48 +0200

    Support multiple guilds checking

--- server.lua
+++ server.lua
@@ -1,4 +1,4 @@
-local GetDiscordID = function(source)
+local function GetDiscordID(source)
     local returnValue = nil
     for idIndex = 1, GetNumPlayerIdentifiers(source) do
         if GetPlayerIdentifier(source, idIndex) ~= nil and GetPlayerIdentifier(source, idIndex):sub(1, #("discord:")) == "discord:" then
@@ -8,37 +8,47 @@ local GetDiscordID = function(source)
     return returnValue
 end
 
-local function GetUserRoles(source)
-    local p = promise.new()
-    PerformHttpRequest(string.format("https://discord.com/api/v10/guilds/%s/members/%s", Config.GuildID, GetDiscordID(source)), function(err, data, headers)
-        if err ~= 200 then
-            p:reject("Error: " .. tostring(err))
-        end
-        p:resolve(json.decode(data).roles)
-
-    end, 'GET', '', { Authorization = "Bot " .. Config.BotToken, ['Content-Type'] = 'application/json' }) 
-    return Citizen.Await(p)
+local function GetUserRoles(disID)
+	local allroles = {}
+	local prom = promise.new() -- PROMISE FOR ALL GUILDS
+    for i = 1, #Config.GuildIDs do -- FOR EACH GUILD
+		local p = promise.new() -- PROMISE FOR EACH INDIVIDUAL GUILD
+		
+		PerformHttpRequest(string.format("https://discord.com/api/v10/guilds/%s/members/%s", Config.GuildIDs[i], disID), function(err, data, headers) -- GET GUILD MEMBER OBJECT FROM DISCORD API
+			if err ~= 200 then
+				p:reject("Error: " .. tostring(err))
+			end
+			p:resolve(json.decode(data).roles) -- RESOLVES PROMISE FOR CURRENT GUILD
+		end, 'GET', '', { Authorization = "Bot " .. Config.BotToken, ['Content-Type'] = 'application/json' })
+		
+		local roles = Citizen.Await(p) -- RETURNS DATA FROM REQUEST
+		for i = 1, #roles do -- SORTING ROLES FROM THE GUILD INTO A SINGLE TABLE
+			table.insert(allroles, roles[i])
+		end
+    end
+	prom:resolve(allroles) -- RESOLVE ALL ROLES AFTER FINISHING EACH GUILD
+	return Citizen.Await(prom) -- RETURN ALL ROLES
 end
 
-local function UserHasRole(source, role)
+local function UserHasRole(disID, role)
     local hasRole = false
-    local roles = GetUserRoles(source)
-    for i = 1, #roles, 1 do
-        if roles[i] == role then
-            hasRole = true
-        end
-    end
+    local data = GetUserRoles(disID) -- GET ALL USER ROLES
+	for i = 1, #data do -- GOES THROUGH EACH ROLE AND CHECKS IF IT IS EQUAL TO THE ROLE SPECIFIED
+		if data[i] == tostring(role) then
+			hasRole = true
+		end
+	end
     return hasRole
 end
 
 exports("GetUserRoles", function(source)
-    return GetUserRoles(source)
+	return GetUserRoles(GetDiscordID(source))
+end)
+
+exports("UserHasRole", function(source, role) 
+    return UserHasRole(GetDiscordID(source), role)
 end)
 
 exports("GetDiscordID", function(source)
     return GetDiscordID(source)
-end)
-
-exports("UserHasRole", function(source, role) 
-    return UserHasRole(source, role)
 end)
