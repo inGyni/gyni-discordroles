7627c2f221c811c33bbdad66c5b22f404c3861ff
Ali <47601790+inGyni@users.noreply.github.com>
2022-12-04 20:40:22 +0200

    Support for multiple guilds checking

--- shared.lua
+++ shared.lua
@@ -1,4 +1,7 @@
 Config = {}
 
 Config.BotToken = "" -- DISCORD BOT TOKEN
-Config.GuildID = "" -- DISCORD SERVER ID (GUILD ID) FOR CHECKING
+Config.GuildIDs = { -- DISCORD SERVER IDs (GUILD ID) FOR CHECKING
+    "",
+    ""
+}
